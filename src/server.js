3f8d2efbf30d27876f91d2337b9917a72ba52374
Vitor rams <161902126+Vitorram@users.noreply.github.com>
2025-02-18 20:47:24 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("è os mlk coringa")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+app.get('/product', (req, res) => {
+  res.send('Nome: Laranja<br> codigo: 132217 <br> tipo: panamera <br> valor: R$ 40')
+
+})
+
+app.listen(port, () => {
+  console.log(`servidor rodando em http://localhost:${port}`)
+})
